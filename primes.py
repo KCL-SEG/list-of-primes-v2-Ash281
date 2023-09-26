01c0c6987e61875d6fde2ae579441b8f65245048
Ash281 <ashvin.valentine281@gmail.com>
2023-09-26 16:52:51 +0100

    primes

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
+    
     list = []
+    num = 2
+    while number_of_primes != 0:
+        isPrime = True
+        for i in range(2, num-1):
+            if num % i == 0:
+                isPrime = False
+        if isPrime == True:
+            list.append(num)
+            number_of_primes -= 1
+        num += 1
     return list
+
